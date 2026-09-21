b01b0bb7e4a1ccb53a8105234570c7bdc393772d
yan-dhsk <yanvianaaa@gmail.com>
2026-09-21 20:08:11 -0300

    refactor: corrige logger em MenuPrincipalAdministrador

--- src/main/java/apresentacao/MenuPrincipalAdministrador.java
+++ src/main/java/apresentacao/MenuPrincipalAdministrador.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 import entidades.Administrador;
 import facade.SistemaFacade;
 
+@SuppressWarnings("java:S106")
 public class MenuPrincipalAdministrador {
 
     private SistemaFacade sistema;
